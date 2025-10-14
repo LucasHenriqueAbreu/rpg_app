bd540cb2ad935bb507897a93f50fae0e9793b85a
LucasHenriqueAbreu <lucasigual14@gmail.com>
2025-10-14 20:40:16 -0300

    aula parte 1 - 14-10-2025

---
+++ lib/presenter/personagens/cadastro_personagem_view.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CadastroPersonagemView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Eu sou o cadastro de personagem');
+  }
+}
--- lib/presenter/personagens/personagens_view.dart
+++ lib/presenter/personagens/personagens_view.dart
@@ -3,6 +3,7 @@ import 'package:rpg_app/domain/entities/anao.dart';
 import 'package:rpg_app/domain/entities/humano.dart';
 import 'package:rpg_app/domain/entities/monstro.dart';
 import 'package:rpg_app/domain/entities/personagem.dart';
+import 'package:rpg_app/presenter/personagens/cadastro_personagem_view.dart';
 
 class PersonagensView extends StatelessWidget {
   PersonagensView({super.key});
@@ -30,6 +31,21 @@ class PersonagensView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: _construirLista(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CadastroPersonagemView()),
+          );
+        },
+      ),
+    );
+  }
+
+  ListView _construirLista() {
     return ListView.builder(
       itemCount: _personagens.length,
       itemBuilder: (context, index) {
