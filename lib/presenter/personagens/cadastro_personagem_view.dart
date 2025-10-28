27d33a3a674956a851bc7cc83524b3d925161f83
LucasHenriqueAbreu <lucasigual14@gmail.com>
2025-10-28 19:23:53 -0300

    ajuste nas imagens

--- lib/presenter/personagens/cadastro_personagem_view.dart
+++ lib/presenter/personagens/cadastro_personagem_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class CadastroPersonagemView extends StatelessWidget {
+  const CadastroPersonagemView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Text('Eu sou o cadastro de personagem');
+    return Text('Cadastro de personagem');
   }
 }
